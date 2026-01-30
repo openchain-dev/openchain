42f49a9c836079e4a24f0478664854520c9e7861
openchain-dev <openchain-dev@users.noreply.github.com>
2026-01-30 16:47:26 +0000

    [CLAW-net-004] [CLAW] build: Implement block sync protocol

---
+++ src/state/blockchain_manager.ts
@@ -0,0 +1,42 @@
+import { Block } from './block';
+import { BlockSyncManager } from '../networking/block_sync_manager';
+import { Peer } from '../networking/peer';
+
+export class BlockchainManager {
+  private blocks: Block[];
+  private blockSyncManager: BlockSyncManager;
+
+  constructor(peers: Peer[]) {
+    this.blocks = [];
+    this.blockSyncManager = new BlockSyncManager(peers, this);
+  }
+
+  async getLatestBlockHash(): Promise<string> {
+    if (this.blocks.length === 0) {
+      return '';
+    }
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+
+  async getMissingBlockHashes(latestHash: string): Promise<string[]> {
+    // Find the index of the latest block in the local blockchain
+    const latestIndex = this.blocks.findIndex((block) => block.hash === latestHash);
+
+    // If the latest block is not found, return all block hashes
+    if (latestIndex === -1) {
+      return this.blocks.map((block) => block.hash);
+    }
+
+    // Otherwise, return the hashes of all blocks after the latest one
+    return this.blocks.slice(latestIndex + 1).map((block) => block.hash);
+  }
+
+  async addBlock(block: Block): Promise<void> {
+    // Implement logic to add a block to the blockchain
+    this.blocks.push(block);
+  }
+
+  async syncBlocks(): Promise<void> {
+    await this.blockSyncManager.syncBlocks();
+  }
+}
