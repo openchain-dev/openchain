fdfcd5c89029846757fef78b5528478b8b97631b
openchain-dev <openchain-dev@users.noreply.github.com>
2026-01-31 14:54:24 +0000

    [CLAW-adv-006] [CLAW] build: Implement account abstraction

--- src/claw-generated/account.ts
+++ src/claw-generated/account.ts
@@ -1,31 +1,22 @@
-import { BigNumber } from 'ethers';
+export interface Account {
+  address: string;
+  balance: BigInt;
+  nonce: number;
+  validateTransaction(tx: Transaction): boolean;
+}
 
-export class Account {
-  public address: string;
-  public balance: BigNumber;
-  public nonce: number;
-  public storageSlots: Map<string, any>;
+export class EOAAccount implements Account {
+  address: string;
+  balance: BigInt;
+  nonce: number;
 
-  constructor(address: string) {
+  constructor(address: string, balance: BigInt, nonce: number) {
     this.address = address;
-    this.balance = BigNumber.from(0);
-    this.nonce = 0;
-    this.storageSlots = new Map();
-  }
-
-  public setBalance(balance: BigNumber): void {
     this.balance = balance;
+    this.nonce = nonce;
   }
 
-  public incrementNonce(): void {
-    this.nonce++;
-  }
-
-  public getStorageSlot(key: string): any {
-    return this.storageSlots.get(key);
-  }
-
-  public setStorageSlot(key: string, value: any): void {
-    this.storageSlots.set(key, value);
+  validateTransaction(tx: Transaction): boolean {
+    return tx.from === this.address && tx.nonce === this.nonce;
   }
 }
