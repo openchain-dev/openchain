15639e7788e57b55d5217ca4cca01bf69d22f356
openchain-dev <openchain-dev@users.noreply.github.com>
2026-01-31 05:59:33 +0000

    [CLAW-adv-005] [CLAW] build: Build oracle system

--- src/claw-generated/Oracle.sol
+++ src/claw-generated/Oracle.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Oracle {
+import "./OracleInterface.sol";
+
+contract Oracle is OracleInterface {
     // Data structure to hold committed data
     struct DataCommitment {
         bytes32 commitment;
@@ -9,35 +11,35 @@ contract Oracle {
     }
 
     // Map of data providers and their commitments
-    mapping(address => DataCommitment) public dataProviders;
+    mapping(address => mapping(bytes32 => DataCommitment)) public dataProviders;
 
     // List of approved data providers
-    address[] public approvedProviders;
+    mapping(address => bool) public approvedProviders;
 
     // Event for data submission
-    event DataSubmitted(address indexed provider, bytes32 indexed commitment, uint256 revealBlock);
+    event DataSubmitted(address indexed provider, bytes32 indexed dataId, bytes32 indexed commitment, uint256 revealBlock);
 
     // Event for provider approval
     event ProviderApproved(address indexed provider);
 
     // Approve a new data provider
-    function approveProvider(address provider) public {
-        require(msg.sender == owner, "Only the owner can approve providers");
-        approvedProviders.push(provider);
+    function approveProvider(address provider) public onlyOwner {
+        require(!approvedProviders[provider], "Provider already approved");
+        approvedProviders[provider] = true;
         emit ProviderApproved(provider);
     }
 
     // Submit a data commitment
-    function submitData(bytes32 commitment, uint256 revealBlock) public {
-        require(isApprovedProvider(msg.sender), "Provider not approved");
-        require(dataProviders[msg.sender].commitment == 0, "Data already committed");
-        dataProviders[msg.sender] = DataCommitment(commitment, revealBlock);
-        emit DataSubmitted(msg.sender, commitment, revealBlock);
+    function submitData(bytes32 dataId, bytes32 commitment, uint256 revealBlock) public {
+        require(approvedProviders[msg.sender], "Provider not approved");
+        require(dataProviders[msg.sender][dataId].commitment == 0, "Data already committed");
+        dataProviders[msg.sender][dataId] = DataCommitment(commitment, revealBlock);
+        emit DataSubmitted(msg.sender, dataId, commitment, revealBlock);
     }
 
     // Reveal the data
-    function revealData(bytes32 data) public {
-        DataCommitment storage commitment = dataProviders[msg.sender];
+    function revealData(bytes32 dataId, bytes32 data) public {
+        DataCommitment storage commitment = dataProviders[msg.sender][dataId];
         require(commitment.commitment != 0, "No data committed");
         require(commitment.revealBlock <= block.number, "Data not ready to reveal");
         require(keccak256(abi.encodePacked(data)) == commitment.commitment, "Invalid data");
@@ -45,13 +47,14 @@ contract Oracle {
         // ...
     }
 
-    // Check if a provider is approved
-    function isApprovedProvider(address provider) public view returns (bool) {
-        for (uint256 i = 0; i < approvedProviders.length; i++) {
-            if (approvedProviders[i] == provider) {
-                return true;
-            }
-        }
-        return false;
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can perform this action");
+        _;
+    }
+
+    address public owner;
+
+    constructor(address _owner) {
+        owner = _owner;
     }
 }
