4ac482f745d6f7d4e5f931c3e30bb93e51b1d5bf
openchain-dev <openchain-dev@users.noreply.github.com>
2026-01-31 01:33:35 +0000

    [CLAW] [CLAW-vm-002] Expand VM with more instructions

--- src/claw-generated/vm.ts
+++ src/claw-generated/vm.ts
@@ -4,6 +4,8 @@ import { Instruction, ContractState } from './types';
 
 class VM {
   private stack: any[] = [];
+  private memory: Map<number, any> = new Map();
+  private pc: number = 0; // program counter
   private state: ContractState;
 
   constructor(initialState: ContractState) {
@@ -11,8 +13,10 @@ class VM {
   }
 
   execute(instructions: Instruction[]) {
-    for (const instruction of instructions) {
+    while (this.pc < instructions.length) {
+      const instruction = instructions[this.pc];
       this.executeInstruction(instruction);
+      this.pc++;
     }
   }
 
@@ -20,6 +24,15 @@ class VM {
     switch (instruction.opcode) {
       case 'PUSH':
         this.stack.push(instruction.operand);
+        break;
+      case 'POP':
+        this.stack.pop();
+        break;
+      case 'LOAD':
+        this.stack.push(this.memory.get(instruction.operand) || 0);
+        break;
+      case 'STORE':
+        this.memory.set(instruction.operand, this.stack.pop());
         break;
       case 'ADD':
         this.stack.push(this.stack.pop() + this.stack.pop());
@@ -29,7 +42,23 @@ class VM {
         const a = this.stack.pop();
         this.stack.push(a - b);
         break;
-      // Add more instruction handlers here
+      case 'MUL':
+        this.stack.push(this.stack.pop() * this.stack.pop());
+        break;
+      case 'DIV':
+        const divisor = this.stack.pop();
+        const dividend = this.stack.pop();
+        this.stack.push(dividend / divisor);
+        break;
+      case 'JUMP':
+        this.pc = instruction.operand;
+        break;
+      case 'JUMPI':
+        const condition = this.stack.pop();
+        if (condition) {
+          this.pc = instruction.operand;
+        }
+        break;
       default:
         throw new Error(`Unknown opcode: ${instruction.opcode}`);
     }
