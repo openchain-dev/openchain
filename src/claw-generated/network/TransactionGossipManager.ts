51c7ea17521ce6702b79dcd4c663d5215a39ad85
openchain-dev <openchain-dev@users.noreply.github.com>
2026-01-31 12:30:01 +0000

    [CLAW-net-003] [CLAW] build: Build transaction gossip protocol

--- src/claw-generated/network/TransactionGossipManager.ts
+++ src/claw-generated/network/TransactionGossipManager.ts
@@ -1,13 +1,16 @@
 import { EventEmitter } from 'events';
-import { Transaction } from '../Transaction';
+import { Transaction } from '../transaction';
 import { PeerManager } from './PeerManager';
+import { TransactionGossipProtocol } from './transaction-gossip';
 
 export class TransactionGossipManager extends EventEmitter {
   private transactionPool: Map<string, Transaction> = new Map();
   private seenTransactions: Set<string> = new Set();
+  private transactionGossipProtocol: TransactionGossipProtocol;
 
   constructor(private peerManager: PeerManager) {
     super();
+    this.transactionGossipProtocol = new TransactionGossipProtocol(this, this.peerManager.getPeers());
   }
 
   async broadcastTransaction(tx: Transaction): Promise<void> {
@@ -18,7 +21,7 @@ export class TransactionGossipManager extends EventEmitter {
     this.transactionPool.set(tx.hash, tx);
     this.seenTransactions.add(tx.hash);
 
-    await this.peerManager.broadcastTransaction(tx);
+    await this.transactionGossipProtocol.broadcastTransaction(tx);
     this.emit('transactionBroadcast', tx);
   }
 
@@ -29,10 +32,15 @@ export class TransactionGossipManager extends EventEmitter {
 
     this.transactionPool.set(tx.hash, tx);
     this.seenTransactions.add(tx.hash);
+    this.transactionGossipProtocol.onTransactionReceived(tx);
     this.emit('newTransaction', tx);
   }
 
   async getTransactionPool(): Promise<Transaction[]> {
     return Array.from(this.transactionPool.values());
+  }
+
+  onPeerConnected(peer: any) {
+    this.transactionGossipProtocol.onPeerConnected(peer);
   }
 }
