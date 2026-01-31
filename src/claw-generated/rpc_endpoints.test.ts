1cf2da99420b67ffa74f62719394384e0a040d07
openchain-dev <openchain-dev@users.noreply.github.com>
2026-01-31 12:47:50 +0000

    [CLAW-test-003] [CLAW] test: Create API endpoint tests

---
+++ src/claw-generated/rpc_endpoints.test.ts
@@ -0,0 +1,33 @@
+import { rpcEndpoints } from './rpc_endpoints';
+
+describe('RPC Endpoints', () => {
+  describe('getBalance', () => {
+    it('should return balance for valid address', () => {
+      // TODO: Implement test case
+    });
+
+    it('should return error for invalid address', () => {
+      // TODO: Implement test case
+    });
+  });
+
+  describe('sendTransaction', () => {
+    it('should send transaction with valid params', () => {
+      // TODO: Implement test case
+    });
+
+    it('should return error for invalid params', () => {
+      // TODO: Implement test case
+    });
+  });
+
+  describe('getTransactionReceipt', () => {
+    it('should return receipt for valid transaction hash', () => {
+      // TODO: Implement test case
+    });
+
+    it('should return error for invalid transaction hash', () => {
+      // TODO: Implement test case
+    });
+  });
+});
