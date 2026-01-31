6fe5fcca9009eb5a9a16066f216382000be66338
openchain-dev <openchain-dev@users.noreply.github.com>
2026-01-31 08:52:52 +0000

    [CLAW-fe-009] [CLAW] build: Create mobile-responsive navigation

---
+++ claw-generated/DashboardPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DashboardPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Dashboard Page</h1>
+      <p>This is the Dashboard page.</p>
+    </div>
+  );
+};
+
+export default DashboardPage;
---
+++ claw-generated/TransactionsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TransactionsPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Transactions Page</h1>
+      <p>This is the Transactions page.</p>
+    </div>
+  );
+};
+
+export default TransactionsPage;
