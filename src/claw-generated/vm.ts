efabdc33c26618ef08c1cea83418ab8d31231a53
openchain-dev <openchain-dev@users.noreply.github.com>
2026-01-31 22:46:10 +0000

    [CLAW-sc-003] [CLAW] build: Build basic VM for contract execution

--- src/claw-generated/vm.ts
+++ src/claw-generated/vm.ts
@@ -40,6 +40,56 @@ export class ClawiVM {
       case 0x02: // POP
         this.stack.pop();
         break;
+      case 0x03: // ADD
+        const b = this.stack.pop();
+        const a = this.stack.pop();
+        this.stack.push(a + b);
+        break;
+      case 0x04: // SUB
+        const b2 = this.stack.pop();
+        const a2 = this.stack.pop();
+        this.stack.push(a2 - b2);
+        break;
+      case 0x05: // MUL
+        const b3 = this.stack.pop();
+        const a3 = this.stack.pop();
+        this.stack.push(a3 * b3);
+        break;
+      case 0x06: // DIV
+        const b4 = this.stack.pop();
+        const a4 = this.stack.pop();
+        this.stack.push(a4 / b4);
+        break;
+      case 0x07: // AND
+        const b5 = this.stack.pop();
+        const a5 = this.stack.pop();
+        this.stack.push(a5 & b5);
+        break;
+      case 0x08: // OR
+        const b6 = this.stack.pop();
+        const a6 = this.stack.pop();
+        this.stack.push(a6 | b6);
+        break;
+      case 0x09: // XOR
+        const b7 = this.stack.pop();
+        const a7 = this.stack.pop();
+        this.stack.push(a7 ^ b7);
+        break;
+      case 0x0A: // NOT
+        const a8 = this.stack.pop();
+        this.stack.push(~a8);
+        break;
+      case 0x0B: // JUMP
+        const destination = this.stack.pop();
+        i = destination;
+        break;
+      case 0x0C: // JUMPI
+        const condition = this.stack.pop();
+        const destination2 = this.stack.pop();
+        if (condition) {
+          i = destination2;
+        }
+        break;
       // Add more opcode handlers here
       default:
         throw new Error(`Unknown opcode: ${opcode}`);
