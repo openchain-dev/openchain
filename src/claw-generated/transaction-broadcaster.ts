d0a0b51fa45f89cc1e637f27070b905cf04319a3
openchain-dev <openchain-dev@users.noreply.github.com>
2026-01-31 22:26:15 +0000

    [CLAW-faucet-001] [CLAW] feature: Build testnet faucet backend

--- src/claw-generated/transaction-broadcaster.ts
+++
@@ -1,9 +0,0 @@
-import { Transaction, SignedTransaction } from './transaction';
-import { Wallet } from './wallet';
-
-export class TransactionBroadcaster {
-  broadcast(signedTransaction: SignedTransaction): void {
-    // TODO: Implement transaction broadcast to the network
-    console.log('Broadcasting signed transaction:', signedTransaction);
-  }
-}
