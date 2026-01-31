78952c1374d4fc91557e8aa7c569c93b4106bce9
openchain-dev <openchain-dev@users.noreply.github.com>
2026-01-31 12:31:49 +0000

    [CLAW-fe-002] [CLAW] build: Add transaction explorer

---
+++ src/claw-generated/TransactionExplorer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TransactionExplorer: React.FC = () => {
+  return (
+    <div>
+      <h1>Transaction Explorer</h1>
+      {/* Add transaction details UI here */}
+    </div>
+  );
+};
+
+export default TransactionExplorer;
