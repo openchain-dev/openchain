07c739a361daf7c7bca4b8a117a2bcafd506df5c
openchain-dev <openchain-dev@users.noreply.github.com>
2026-01-31 11:10:58 +0000

    [CLAW-sc-002] [CLAW] build: Implement NFT standard (CRC-721)

--- src/claw-generated/CRC721.sol
+++ src/claw-generated/CRC721.sol
@@ -1,42 +1,167 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./ICRC721.sol";
 
-contract CRC721 is ERC721, Ownable {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIdCounter;
+/**
+ * @title CRC721 Non-Fungible Token Standard
+ * @dev Implementation of the basic CRC721 standard.
+ */
+contract CRC721 is ICRC721 {
+    // Token name
+    string private _name;
 
-    mapping(uint256 => string) private _tokenURIs;
+    // Token symbol
+    string private _symbol;
 
-    constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
+    // Mapping from token ID to owner address
+    mapping(uint256 => address) private _owners;
 
-    function safeMint(address to) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
+    // Mapping from owner to token count
+    mapping(address => uint256) private _balances;
+
+    // Mapping from token ID to approved address
+    mapping(uint256 => address) private _tokenApprovals;
+
+    // Mapping from owner to operator approvals
+    mapping(address => mapping(address => bool)) private _operatorApprovals;
+
+    constructor(string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
     }
 
-    function burn(uint256 tokenId) public {
-        require(_exists(tokenId), "CRC721: token does not exist");
-        require(ownerOf(tokenId) == msg.sender, "CRC721: not owner of token");
-        _burn(tokenId);
+    function balanceOf(address owner) public view returns (uint256) {
+        require(owner != address(0), "CRC721: balance query for the zero address");
+        return _balances[owner];
     }
 
-    function transferFrom(address from, address to, uint256 tokenId) public virtual override {
-        require(_isApprovedOrOwner(_msgSender(), tokenId), "CRC721: caller is not owner nor approved");
+    function ownerOf(uint256 tokenId) public view returns (address) {
+        address owner = _owners[tokenId];
+        require(owner != address(0), "CRC721: owner query for nonexistent token");
+        return owner;
+    }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public virtual {
+        safeTransferFrom(from, to, tokenId, "");
+    }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory data
+    ) public virtual {
+        transferFrom(from, to, tokenId);
+        require(_checkOnCRC721Received(from, to, tokenId, data), "CRC721: transfer to non CRC721Receiver implementer");
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public virtual {
+        //solhint-disable-next-line max-line-length
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "CRC721: transfer caller is not owner nor approved");
+
         _transfer(from, to, tokenId);
     }
 
-    function setTokenURI(uint256 tokenId, string memory uri) public onlyOwner {
-        require(_exists(tokenId), "CRC721: token does not exist");
-        _tokenURIs[tokenId] = uri;
+    function approve(address to, uint256 tokenId) public virtual {
+        address owner = ownerOf(tokenId);
+        require(to != owner, "CRC721: approval to current owner");
+
+        require(
+            _msgSender() == owner || isApprovedForAll(owner, _msgSender()),
+            "CRC721: approve caller is not owner nor approved for all"
+        );
+
+        _approve(to, tokenId);
     }
 
-    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-        require(_exists(tokenId), "CRC721: token does not exist");
-        return _tokenURIs[tokenId];
+    function getApproved(uint256 tokenId) public view virtual returns (address) {
+        require(_exists(tokenId), "CRC721: approved query for nonexistent token");
+
+        return _tokenApprovals[tokenId];
+    }
+
+    function setApprovalForAll(address operator, bool approved) public virtual {
+        require(operator != _msgSender(), "CRC721: approve to caller");
+
+        _operatorApprovals[_msgSender()][operator] = approved;
+        emit ApprovalForAll(_msgSender(), operator, approved);
+    }
+
+    function isApprovedForAll(address owner, address operator) public view virtual returns (bool) {
+        return _operatorApprovals[owner][operator];
+    }
+
+    function _transfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal virtual {
+        require(ownerOf(tokenId) == from, "CRC721: transfer of token that is not own");
+        require(to != address(0), "CRC721: transfer to the zero address");
+
+        _beforeTokenTransfer(from, to, tokenId);
+
+        // Clear approvals from the previous owner
+        _approve(address(0), tokenId);
+
+        _balances[from] -= 1;
+        _balances[to] += 1;
+        _owners[tokenId] = to;
+
+        emit Transfer(from, to, tokenId);
+    }
+
+    function _approve(address to, uint256 tokenId) internal virtual {
+        _tokenApprovals[tokenId] = to;
+        emit Approval(ownerOf(tokenId), to, tokenId);
+    }
+
+    function _exists(uint256 tokenId) internal view virtual returns (bool) {
+        return _owners[tokenId] != address(0);
+    }
+
+    function _isApprovedOrOwner(address spender, uint256 tokenId) internal view virtual returns (bool) {
+        require(_exists(tokenId), "CRC721: operator query for nonexistent token");
+        address owner = ownerOf(tokenId);
+        return (spender == owner || getApproved(tokenId) == spender || isApprovedForAll(owner, spender));
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal virtual {}
+
+    function _checkOnCRC721Received(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory data
+    ) private returns (bool) {
+        if (to.isContract()) {
+            try ICRC721Receiver(to).onCRC721Received(_msgSender(), from, tokenId, data) returns (bytes4 retval) {
+                return retval == ICRC721Receiver.onCRC721Received.selector;
+            } catch (bytes memory reason) {
+                if (reason.length == 0) {
+                    revert("CRC721: transfer to non CRC721Receiver implementer");
+                } else {
+                    // solhint-disable-next-line no-inline-assembly
+                    assembly {
+                        revert(add(32, reason), mload(reason))
+                    }
+                }
+            }
+        } else {
+            return true;
+        }
     }
 }
