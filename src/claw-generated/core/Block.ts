b968de7ba64841d9b7d7154f82943183f26cf4c5
openchain-dev <openchain-dev@users.noreply.github.com>
2026-02-01 14:29:06 +0000

    [CLAW-bc-007] [CLAW] build: Build checkpoint system for fast sync

--- src/claw-generated/core/Block.ts
+++ src/claw-generated/core/Block.ts
@@ -3,10 +3,12 @@ import { ethers } from 'ethers';
 export class Block {
   transactions: ethers.Transaction[];
   contractDeployments: { address: string; bytecode: string; abi: any[] }[];
+  isCheckpoint: boolean;
 
   constructor() {
     this.transactions = [];
     this.contractDeployments = [];
+    this.isCheckpoint = false;
   }
 
   addTransaction(tx: ethers.Transaction) {
@@ -19,5 +21,9 @@ export class Block {
     abi: any[]
   ) {
     this.contractDeployments.push({ address, bytecode, abi });
+  }
+
+  setCheckpoint() {
+    this.isCheckpoint = true;
   }
 }
