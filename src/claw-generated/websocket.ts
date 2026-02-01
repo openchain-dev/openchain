045ed4766b5255dcb60e2a649d62b49e72ef9715
openchain-dev <openchain-dev@users.noreply.github.com>
2026-02-01 07:39:47 +0000

    [CLAW-api-009] [CLAW] build: Build WebSocket subscription system

--- src/claw-generated/websocket.ts
+++ src/claw-generated/websocket.ts
@@ -1,28 +1,77 @@
 import WebSocket from 'ws';
-import { logger } from '../utils/logger';
-import { getLatestBlock, subscribeToLogs, subscribeToPendingTransactions } from '../blockchain';
+import { logger } from './utils/logger';
+import { Blockchain } from './blockchain/blockchain';
+import { TransactionPool } from './transaction/transaction_mempool';
+import { AccountStorage } from './AccountStorage';
 
 const wss = new WebSocket.Server({ port: 8080 });
 
+const blockchain = new Blockchain();
+const transactionPool = new TransactionPool();
+const accountStorage = new AccountStorage();
+
+const subscriptions: { [id: string]: { type: string; data: any; callback: (data: any) => void } } = {};
+
 wss.on('connection', (ws) => {
   logger.info('WebSocket client connected');
+  let clientId = 0;
 
   ws.on('message', (message) => {
     try {
-      const { type, data } = JSON.parse(message);
+      const { type, data, id } = JSON.parse(message);
       switch (type) {
-        case 'newHeads':
-          ws.send(JSON.stringify({ type: 'newHeads', data: getLatestBlock() }));
+        case 'newBlock':
+          subscriptions[id] = {
+            type: 'newBlock',
+            data: null,
+            callback: (block) => {
+              ws.send(JSON.stringify({ type: 'newBlock', data: block, id }));
+            },
+          };
+          blockchain.subscribeToNewBlocks(subscriptions[id].callback);
           break;
-        case 'logs':
-          subscribeToLogs(data, (logs) => {
-            ws.send(JSON.stringify({ type: 'logs', data: logs }));
-          });
+        case 'transactionConfirmed':
+          subscriptions[id] = {
+            type: 'transactionConfirmed',
+            data: data.address,
+            callback: (transactions) => {
+              ws.send(JSON.stringify({ type: 'transactionConfirmed', data: transactions, id }));
+            },
+          };
+          transactionPool.subscribeToTransactionConfirmations(data.address, subscriptions[id].callback);
           break;
-        case 'pendingTransactions':
-          subscribeToPendingTransactions((transactions) => {
-            ws.send(JSON.stringify({ type: 'pendingTransactions', data: transactions }));
-          });
+        case 'accountUpdated':
+          subscriptions[id] = {
+            type: 'accountUpdated',
+            data: data.address,
+            callback: (account) => {
+              ws.send(JSON.stringify({ type: 'accountUpdated', data: account, id }));
+            },
+          };
+          accountStorage.subscribeToAccountUpdates(data.address, subscriptions[id].callback);
+          break;
+        case 'unsubscribe':
+          const subscription = subscriptions[data.id];
+          if (subscription) {
+            switch (subscription.type) {
+              case 'newBlock':
+                blockchain.unsubscribeFromNewBlocks(subscription.callback);
+                break;
+              case 'transactionConfirmed':
+                transactionPool.unsubscribeFromTransactionConfirmations(
+                  subscription.data,
+                  subscription.callback
+                );
+                break;
+              case 'accountUpdated':
+                accountStorage.unsubscribeFromAccountUpdates(
+                  subscription.data,
+                  subscription.callback
+                );
+                break;
+            }
+            delete subscriptions[data.id];
+          }
           break;
         default:
           logger.warn(`Unsupported subscription type: ${type}`);
@@ -34,6 +83,29 @@ wss.on('connection', (ws) => {
 
   ws.on('close', () => {
     logger.info('WebSocket client disconnected');
+    // Clean up any active subscriptions for this client
+    Object.keys(subscriptions).forEach((id) => {
+      if (subscriptions[id].callback) {
+        switch (subscriptions[id].type) {
+          case 'newBlock':
+            blockchain.unsubscribeFromNewBlocks(subscriptions[id].callback);
+            break;
+          case 'transactionConfirmed':
+            transactionPool.unsubscribeFromTransactionConfirmations(
+              subscriptions[id].data,
+              subscriptions[id].callback
+            );
+            break;
+          case 'accountUpdated':
+            accountStorage.unsubscribeFromAccountUpdates(
+              subscriptions[id].data,
+              subscriptions[id].callback
+            );
+            break;
+        }
+      }
+      delete subscriptions[id];
+    });
   });
 });
 
