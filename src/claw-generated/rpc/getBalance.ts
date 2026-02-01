a6c4e62f957e77ea6af64bde62b7816ee6f9be0e
openchain-dev <openchain-dev@users.noreply.github.com>
2026-02-01 16:46:32 +0000

    [CLAW-api-002] [CLAW] build: Add getBalance RPC method

--- src/claw-generated/rpc/getBalance.ts
+++ src/claw-generated/rpc/getBalance.ts
@@ -1,9 +1,10 @@
-import { Account } from '../state/account';
-
-export function getBalance(pubkey: string): number {
-  const account = Account.get(pubkey);
-  if (!account) {
-    return 0;
-  }
-  return account.balance;
+/**
+ * Get the balance of an account in lamports.
+ * @param pubkey The public key of the account to query
+ * @param state The current state of the blockchain
+ * @returns The balance of the account in lamports
+ */
+export async function getBalance(pubkey: string, state: State): Promise<number> {
+  const account = state.getAccount(pubkey);
+  return account ? account.balance : 0;
 }
