4738c53b362e45883b5132f83c77243f1713970c
openchain-dev <openchain-dev@users.noreply.github.com>
2026-02-01 08:30:20 +0000

    [CLAW-sc-003] [CLAW] build: Build basic VM for contract execution

--- src/claw-generated/vm/index.ts
+++ src/claw-generated/vm/index.ts
@@ -1,38 +1,32 @@
-export class VirtualMachine {
-  private gasLimit: number;
-  private gasUsed: number;
+import { Instruction, MemorySlot, Stack } from './types';
 
-  constructor(gasLimit: number) {
-    this.gasLimit = gasLimit;
-    this.gasUsed = 0;
-  }
+class VirtualMachine {
+  private stack: Stack = [];
+  private memory: MemorySlot[] = [];
 
-  execute(bytecode: Uint8Array): void {
-    for (let i = 0; i &lt; bytecode.length; i++) {
-      const opcode = bytecode[i];
-      this.gasUsed += this.getGasCost(opcode);
-      if (this.gasUsed > this.gasLimit) {
-        throw new Error('Out of gas');
-      }
-      // TODO: Implement opcode execution
+  execute(instructions: Instruction[]) {
+    for (const instruction of instructions) {
+      this.executeInstruction(instruction);
     }
   }
 
-  private getGasCost(opcode: number): number {
-    switch (opcode) {
-      case 0x00: // STOP
-        return 0;
-      case 0x01: // ADD
-        return 3;
-      case 0x02: // MUL
-        return 5;
-      // Add more opcodes and their gas costs
-      default:
-        return 1; // Default gas cost for unknown opcodes
+  private executeInstruction(instruction: Instruction) {
+    switch (instruction.opcode) {
+      case 'PUSH':
+        this.stack.push(instruction.operand);
+        break;
+      case 'POP':
+        this.stack.pop();
+        break;
+      case 'ADD':
+        this.stack.push(this.stack.pop() + this.stack.pop());
+        break;
+      case 'MUL':
+        this.stack.push(this.stack.pop() * this.stack.pop());
+        break;
+      // Add more instruction handlers here
     }
-  }
-
-  getGasUsed(): number {
-    return this.gasUsed;
   }
 }
+
+export { VirtualMachine };
