dbd3e40a0560c32894f4dd562c73a753f5dc3119
openchain-dev <openchain-dev@users.noreply.github.com>
2026-02-01 11:44:29 +0000

    [CLAW-sc-001] [CLAW] build: Design CLAW token standard (CRC-20)

--- claw-generated/CRC20.sol
+++ claw-generated/CRC20.sol
@@ -3,26 +3,37 @@ pragma solidity ^0.8.0;
 
 import "./ICRC20.sol";
 
+/**
+ * @dev Implementation of the CRC20 token standard as defined in the EIP.
+ */
 contract CRC20 is ICRC20 {
-    string public name;
-    string public symbol;
-    uint8 public decimals;
+    mapping(address =&gt; uint256) private _balances;
+    mapping(address =&gt; mapping(address =&gt; uint256)) private _allowances;
+
     uint256 private _totalSupply;
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
 
-    mapping(address => uint256) private _balances;
-    mapping(address => mapping(address => uint256)) private _allowances;
-
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        uint8 _decimals,
-        uint256 initialSupply
-    ) {
-        name = _name;
-        symbol = _symbol;
-        decimals = _decimals;
-        _totalSupply = initialSupply * 10 ** uint256(decimals);
+    constructor(string memory name_, string memory symbol_, uint8 decimals_, uint256 initialSupply) {
+        _name = name_;
+        _symbol = symbol_;
+        _decimals = decimals_;
+        _totalSupply = initialSupply * 10 ** uint256(_decimals);
         _balances[msg.sender] = _totalSupply;
+        emit Transfer(address(0), msg.sender, _totalSupply);
+    }
+
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public view returns (uint8) {
+        return _decimals;
     }
 
     function totalSupply() public view override returns (uint256) {
@@ -47,43 +58,26 @@ contract CRC20 is ICRC20 {
         return true;
     }
 
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) public virtual override returns (bool) {
+    function transferFrom(address sender, address recipient, uint256 amount) public virtual override returns (bool) {
         _transfer(sender, recipient, amount);
         _approve(sender, _msgSender(), _allowances[sender][_msgSender()] - amount);
         return true;
     }
 
-    function _transfer(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) internal virtual {
+    function _transfer(address sender, address recipient, uint256 amount) internal virtual {
         require(sender != address(0), "CRC20: transfer from the zero address");
         require(recipient != address(0), "CRC20: transfer to the zero address");
 
         _balances[sender] -= amount;
         _balances[recipient] += amount;
-
         emit Transfer(sender, recipient, amount);
     }
 
-    function _approve(
-        address owner,
-        address spender,
-        uint256 amount
-    ) internal virtual {
+    function _approve(address owner, address spender, uint256 amount) internal virtual {
         require(owner != address(0), "CRC20: approve from the zero address");
         require(spender != address(0), "CRC20: approve to the zero address");
 
         _allowances[owner][spender] = amount;
         emit Approval(owner, spender, amount);
-    }
-
-    function _msgSender() internal view virtual returns (address) {
-        return msg.sender;
     }
 }
