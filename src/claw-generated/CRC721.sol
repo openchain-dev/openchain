30013463ac243d3a73c0042cc3fe39aff1d76096
openchain-dev <openchain-dev@users.noreply.github.com>
2026-02-01 12:17:54 +0000

    [CLAW] Implement CRC-721 NFT standard

--- src/claw-generated/CRC721.sol
+++ src/claw-generated/CRC721.sol
@@ -1,158 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./ERC165.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-/**
- * @dev Implementation of the CRC-721 Non-Fungible Token Standard.
- */
-contract CRC721 is ERC165 {
-    // Token name
-    string public name;
+contract CRC721 is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
 
-    // Token symbol
-    string public symbol;
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
 
-    // Mapping from token ID to owner address
-    mapping(uint256 => address) private _owners;
-
-    // Mapping from token ID to approved address
-    mapping(uint256 => address) private _tokenApprovals;
-
-    // Mapping from owner to operator approvals
-    mapping(address => mapping(address => bool)) private _operatorApprovals;
-
-    // Mapping from token ID to token URI
-    mapping(uint256 => string) private _tokenURIs;
-
-    // Total number of tokens
-    uint256 private _totalSupply;
-
-    constructor(string memory name_, string memory symbol_) {
-        name = name_;
-        symbol = symbol_;
+    function safeMint(address to, string memory tokenURI) public {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, tokenURI);
     }
 
-    /**
-     * @dev See {IERC165-supportsInterface}.
-     */
-    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
-        return
-            interfaceId == type(IERC721).interfaceId ||
-            interfaceId == type(IERC721Metadata).interfaceId ||
-            super.supportsInterface(interfaceId);
-    }
-
-    /**
-     * @dev Mints a new token with the given tokenId.
-     * @param to The address that will own the minted token.
-     * @param tokenId The ID of the token to mint.
-     */
-    function mint(address to, uint256 tokenId) public {
-        require(_owners[tokenId] == address(0), "Token already exists");
-        _owners[tokenId] = to;
-        _totalSupply++;
-    }
-
-    /**
-     * @dev Burns the token with the given tokenId.
-     * @param tokenId The ID of the token to burn.
-     */
     function burn(uint256 tokenId) public {
-        address owner = _owners[tokenId];
-        require(owner != address(0), "Token does not exist");
-        require(owner == msg.sender || isApprovedForAll(owner, msg.sender), "Not authorized");
-        _owners[tokenId] = address(0);
-        _totalSupply--;
+        require(_exists(tokenId), "CRC721: token does not exist");
+        require(ownerOf(tokenId) == msg.sender, "CRC721: caller is not owner");
+        _burn(tokenId);
     }
 
-    /**
-     * @dev Transfers the token with the given tokenId from the sender to the recipient.
-     * @param from The address that currently owns the token.
-     * @param to The address that will receive the token.
-     * @param tokenId The ID of the token to transfer.
-     */
-    function transferFrom(address from, address to, uint256 tokenId) public {
-        address owner = _owners[tokenId];
-        require(owner != address(0), "Token does not exist");
-        require(owner == from, "Not the owner");
-        require(to != address(0), "Cannot transfer to the zero address");
-        require(
-            from == msg.sender || isApprovedForAll(from, msg.sender) || _tokenApprovals[tokenId] == msg.sender,
-            "Not authorized"
-        );
-        _owners[tokenId] = to;
-        _tokenApprovals[tokenId] = address(0);
+    function transferFrom(address from, address to, uint256 tokenId) public virtual override {
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "CRC721: caller is not owner nor approved");
+        _transfer(from, to, tokenId);
     }
 
-    /**
-     * @dev Approves the given address to transfer the specified token on behalf of the caller.
-     * @param to The address to be approved for the given token ID.
-     * @param tokenId The ID of the token to be approved.
-     */
-    function approve(address to, uint256 tokenId) public {
-        address owner = _owners[tokenId];
-        require(owner != address(0), "Token does not exist");
-        require(owner == msg.sender || isApprovedForAll(owner, msg.sender), "Not authorized");
-        _tokenApprovals[tokenId] = to;
-    }
-
-    /**
-     * @dev Sets or unsets the approval of a given operator.
-     * @param operator The operator address to set the approval.
-     * @param approved True if the operation can be approved, false to revoke the approval.
-     */
-    function setApprovalForAll(address operator, bool approved) public {
-        _operatorApprovals[msg.sender][operator] = approved;
-    }
-
-    /**
-     * @dev Returns whether the given spender is approved to transfer the given token on behalf of the owner.
-     * @param owner The address that owns the token.
-     * @param operator The address that acts on behalf of the owner.
-     * @return bool True if the operator is approved, false otherwise.
-     */
-    function isApprovedForAll(address owner, address operator) public view returns (bool) {
-        return _operatorApprovals[owner][operator];
-    }
-
-    /**
-     * @dev Returns the total number of tokens in existence.
-     * @return uint256 The total number of tokens.
-     */
-    function totalSupply() public view returns (uint256) {
-        return _totalSupply;
-    }
-
-    /**
-     * @dev Returns the owner of the specified token ID.
-     * @param tokenId The ID of the token to query the owner of.
-     * @return address The owner of the specified token ID.
-     */
-    function ownerOf(uint256 tokenId) public view returns (address) {
-        address owner = _owners[tokenId];
-        require(owner != address(0), "Token does not exist");
-        return owner;
-    }
-
-    /**
-     * @dev Sets the token URI for the specified token ID.
-     * @param tokenId The ID of the token.
-     * @param tokenURI The URI of the token metadata.
-     */
-    function setTokenURI(uint256 tokenId, string memory tokenURI) public {
-        require(_owners[tokenId] != address(0), "Token does not exist");
-        require(_owners[tokenId] == msg.sender || isApprovedForAll(_owners[tokenId], msg.sender), "Not authorized");
-        _tokenURIs[tokenId] = tokenURI;
-    }
-
-    /**
-     * @dev Returns the token URI of the specified token ID.
-     * @param tokenId The ID of the token.
-     * @return string The token URI.
-     */
-    function tokenURI(uint256 tokenId) public view returns (string memory) {
-        require(_owners[tokenId] != address(0), "Token does not exist");
-        return _tokenURIs[tokenId];
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public virtual override {
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "CRC721: caller is not owner nor approved");
+        _safeTransfer(from, to, tokenId, data);
     }
 }
--- src/claw-generated/CRC721.test.ts
+++ src/claw-generated/CRC721.test.ts
@@ -1,50 +1,34 @@
+import { expect } from 'chai';
 import { ethers } from 'ethers';
 import { CRC721 } from './CRC721';
 
 describe('CRC721', () => {
-  let crc721: CRC721;
-  let provider: ethers.providers.Provider;
-  let owner: ethers.Wallet;
-  let user1: ethers.Wallet;
+  let contract: CRC721;
+  let owner: ethers.Signer;
+  let user1: ethers.Signer;
 
   beforeEach(async () => {
-    provider = new ethers.providers.JsonRpcProvider();
-    owner = new ethers.Wallet(process.env.OWNER_PRIVATE_KEY!, provider);
-    user1 = new ethers.Wallet(process.env.USER1_PRIVATE_KEY!, provider);
-
-    const contractAddress = '0x1234567890123456789012345678901234567890';
-    crc721 = new CRC721(contractAddress, provider);
+    [owner, user1] = await ethers.getSigners();
+    contract = await new CRC721('Test NFT', 'TNFT').deploy();
   });
 
-  it('should mint a new NFT', async () => {
-    const tokenUri = 'ipfs://QmXoypizjW3WknFiJnKLwHCnL72vedxjQkDDP1mXWo6uco/metadata.json';
-    const tx = await crc721.safeMint(user1.address, tokenUri);
-    await tx.wait();
-
-    const tokenId = 0;
-    expect(await crc721.tokenURI(tokenId)).toEqual(tokenUri);
-    expect(await provider.getBalance(user1.address)).toEqual(ethers.constants.WeiPerEther.mul(10000));
+  it('should mint a new token', async () => {
+    await contract.connect(owner).safeMint(await owner.getAddress(), 'ipfs://test-uri');
+    expect(await contract.balanceOf(await owner.getAddress())).to.equal(1);
   });
 
-  it('should burn an NFT', async () => {
-    const tokenUri = 'ipfs://QmXoypizjW3WknFiJnKLwHCnL72vedxjQkDDP1mXWo6uco/metadata.json';
-    await crc721.safeMint(user1.address, tokenUri);
-
-    const tokenId = 0;
-    const tx = await crc721.burn(tokenId);
-    await tx.wait();
-
-    await expect(crc721.tokenURI(tokenId)).rejects.toThrow();
+  it('should burn a token', async () => {
+    const tokenId = await contract.connect(owner).callStatic.safeMint(await owner.getAddress(), 'ipfs://test-uri');
+    await contract.connect(owner).safeMint(await owner.getAddress(), 'ipfs://test-uri');
+    await contract.connect(owner).burn(tokenId);
+    expect(await contract.balanceOf(await owner.getAddress())).to.equal(1);
   });
 
-  it('should transfer an NFT', async () => {
-    const tokenUri = 'ipfs://QmXoypizjW3WknFiJnKLwHCnL72vedxjQkDDP1mXWo6uco/metadata.json';
-    await crc721.safeMint(user1.address, tokenUri);
-
-    const tokenId = 0;
-    const tx = await crc721.transferFrom(user1.address, owner.address, tokenId);
-    await tx.wait();
-
-    expect(await crc721.ownerOf(tokenId)).toEqual(owner.address);
+  it('should transfer a token', async () => {
+    const tokenId = await contract.connect(owner).callStatic.safeMint(await owner.getAddress(), 'ipfs://test-uri');
+    await contract.connect(owner).safeMint(await owner.getAddress(), 'ipfs://test-uri');
+    await contract.connect(owner).transferFrom(await owner.getAddress(), await user1.getAddress(), tokenId);
+    expect(await contract.balanceOf(await owner.getAddress())).to.equal(1);
+    expect(await contract.balanceOf(await user1.getAddress())).to.equal(1);
   });
 });
