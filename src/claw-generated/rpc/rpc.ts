20b877288a172e1fbc27c7d89faff18029cd357a
openchain-dev <openchain-dev@users.noreply.github.com>
2026-02-01 15:14:30 +0000

    [CLAW-api-003] [CLAW] build: Add getTransaction RPC method

--- src/claw-generated/rpc/rpc.ts
+++ src/claw-generated/rpc/rpc.ts
@@ -1,18 +1,22 @@
 import { GetAccountInfoRpcImpl, GetAccountInfoRpc } from './get_account_info';
 import { GetBlockRpcImpl } from './get_block';
+import { GetTransactionRpcImpl } from './get_transaction';
 import { AccountManager, AccountStorage } from '../AccountManager';
 import { BlockManager } from '../BlockManager';
+import { TransactionManager } from '../TransactionManager';
 import { createMethodHandler, JsonRpcServer } from './server';
 
 export class RpcServer extends JsonRpcServer {
   constructor(
     private accountManager: AccountManager,
     private accountStorage: AccountStorage,
-    private blockManager: BlockManager
+    private blockManager: BlockManager,
+    private transactionManager: TransactionManager
   ) {
     super();
     this.registerMethod('getAccountInfo', this.getAccountInfo.bind(this));
     this.registerMethod('getBlock', this.getBlock.bind(this));
+    this.registerMethod('getTransaction', this.getTransaction.bind(this));
   }
 
   private getAccountInfo = createMethodHandler(
@@ -28,6 +32,14 @@ export class RpcServer extends JsonRpcServer {
       const { slot } = params;
       const impl = new GetBlockRpcImpl(this.blockManager);
       return await impl.get_block(slot);
+    }
+  );
+
+  private getTransaction = createMethodHandler(
+    async (params: { signature: string }) => {
+      const { signature } = params;
+      const impl = new GetTransactionRpcImpl(this.transactionManager);
+      return await impl.get_transaction(signature);
     }
   );
 }
