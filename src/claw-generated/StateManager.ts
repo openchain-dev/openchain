601d803288fe2ec156ab0a95f20aa314dafa031e
openchain-dev <openchain-dev@users.noreply.github.com>
2026-02-01 02:56:27 +0000

    [CLAW] [CLAW-adv-008] Implement state snapshot system

--- src/claw-generated/StateManager.ts
+++ src/claw-generated/StateManager.ts
@@ -1,10 +1,12 @@
 import { BlockHeader } from '../chain/BlockHeader';
 import { Account } from './Account';
+import { serialize, deserialize } from './utils/serialization';
 
 export class StateManager {
   private state: { [key: string]: Account } = {};
   private previousState: { [key: string]: Account } = {};
   private stateDiffs: { [blockHash: string]: { [address: string]: Account } } = {};
+  private stateSnapshots: { [snapshotHash: string]: Uint8Array } = {};
 
   applyBlockChanges(blockHeader: BlockHeader) {
     // Store the current state as the previous state
@@ -23,6 +25,11 @@ export class StateManager {
 
     // Generate the state diff
     this.stateDiffs[blockHeader.hash] = this.getDiff(this.previousState, this.state);
+
+    // Take a state snapshot every 10 blocks
+    if (Object.keys(this.stateDiffs).length % 10 === 0) {
+      this.takeStateSnapshot(blockHeader.hash);
+    }
   }
 
   getAccount(address: string): Account {
@@ -49,5 +56,22 @@ export class StateManager {
     }
 
     return diff;
+  }
+
+  private takeStateSnapshot(snapshotHash: string) {
+    const serializedState = serialize(this.state);
+    this.stateSnapshots[snapshotHash] = serializedState;
+  }
+
+  loadStateSnapshot(snapshotHash: string) {
+    const serializedState = this.stateSnapshots[snapshotHash];
+    this.state = deserialize(serializedState);
+  }
+
+  applyStateDiffs(blockHeader: BlockHeader) {
+    const stateDiff = this.stateDiffs[blockHeader.hash];
+    for (const address in stateDiff) {
+      this.updateAccount(address, stateDiff[address]);
+    }
   }
 }
