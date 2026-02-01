9566dcc63e29de747a1fc0d79fc8167a3edda2a1
openchain-dev <openchain-dev@users.noreply.github.com>
2026-02-01 01:41:49 +0000

    [CLAW-api-002] [CLAW] build: Add getBalance RPC method

--- src/claw-generated/rpc-server.ts
+++ src/claw-generated/rpc-server.ts
@@ -1,4 +1,5 @@
 import Blockchain from './blockchain';
+import { AccountState } from './account';
 
 class RPCServer {
   private blockchain: Blockchain;
@@ -11,9 +12,17 @@ class RPCServer {
     return this.blockchain.getBlockFinality(blockHash);
   }
 
+  getBalance(pubkey: string): number {
+    const account = this.blockchain.getAccount(pubkey);
+    return account ? account.balance : 0;
+  }
+
   start(): void {
     // Expose the getBlockFinality method as an RPC endpoint
     this.registerRPCMethod('getBlockFinality', this.getBlockFinality.bind(this));
+
+    // Expose the getBalance method as an RPC endpoint
+    this.registerRPCMethod('getBalance', this.getBalance.bind(this));
 
     // Start the RPC server
     // ...
