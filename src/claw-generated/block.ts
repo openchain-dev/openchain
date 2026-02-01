cc407ed850564d1dc1c46fa7b83e29d46a4b22d6
openchain-dev <openchain-dev@users.noreply.github.com>
2026-02-01 05:00:25 +0000

    [CLAW-bc-007] [CLAW] build: Build checkpoint system for fast sync

--- src/claw-generated/block.ts
+++ src/claw-generated/block.ts
@@ -8,13 +8,19 @@ export class Block {
   previousHash: string;
   hash: string;
   size: number;
+  isCheckpoint: boolean;
+  checkpointIndex: number;
+  checkpointHash: string;
 
   constructor(
     index: number,
     timestamp: number,
     transactions: Transaction[],
     previousHash: string,
-    hash: string
+    hash: string,
+    isCheckpoint: boolean,
+    checkpointIndex: number,
+    checkpointHash: string
   ) {
     this.index = index;
     this.timestamp = timestamp;
@@ -22,6 +28,9 @@ export class Block {
     this.previousHash = previousHash;
     this.hash = hash;
     this.size = this.calculateSize();
+    this.isCheckpoint = isCheckpoint;
+    this.checkpointIndex = checkpointIndex;
+    this.checkpointHash = checkpointHash;
   }
 
   calculateSize(): number {
@@ -30,7 +39,10 @@ export class Block {
       Buffer.byteLength(this.index.toString()) +
       Buffer.byteLength(this.timestamp.toString()) +
       Buffer.byteLength(this.previousHash) +
-      Buffer.byteLength(this.hash);
+      Buffer.byteLength(this.hash) +
+      Buffer.byteLength(this.isCheckpoint.toString()) +
+      Buffer.byteLength(this.checkpointIndex.toString()) +
+      Buffer.byteLength(this.checkpointHash);
 
     const transactionSize = this.transactions.reduce((total, tx) => {
       return total + tx.size;
