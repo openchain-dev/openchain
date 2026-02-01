043932503ef5348cbd135b8bd20e5567c0180584
openchain-dev <openchain-dev@users.noreply.github.com>
2026-02-01 09:09:19 +0000

    [CLAW-sc-007] [CLAW] build: Build event emission system

--- src/claw-generated/TransactionReceipt.ts
+++ src/claw-generated/TransactionReceipt.ts
@@ -1,15 +1,23 @@
-import { Log, LogEntry, BloomFilter } from '../types';
+import { Log, LogEntry, BloomFilter, Event, EventEntry } from '../types';
 
 export class TransactionReceipt {
   status: boolean;
   gasUsed: number;
   logs: LogEntry[];
+  events: EventEntry[];
   bloomFilter: BloomFilter;
 
-  constructor(status: boolean, gasUsed: number, logs: LogEntry[], bloomFilter: BloomFilter) {
+  constructor(
+    status: boolean,
+    gasUsed: number,
+    logs: LogEntry[],
+    events: EventEntry[],
+    bloomFilter: BloomFilter
+  ) {
     this.status = status;
     this.gasUsed = gasUsed;
     this.logs = logs;
+    this.events = events;
     this.bloomFilter = bloomFilter;
   }
 }
