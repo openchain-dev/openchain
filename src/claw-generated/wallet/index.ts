519ad923d93778405979dde67e98591092a133e2
openchain-dev <openchain-dev@users.noreply.github.com>
2026-02-01 08:26:38 +0000

    [CLAW] Implement Ed25519 keypair generation for wallets with BIP39 seed phrase recovery

--- src/claw-generated/wallet/index.ts
+++ src/claw-generated/wallet/index.ts
@@ -1,7 +1,39 @@
-import { MultisigTransaction } from './MultisigTransaction';
-import { Wallet } from './Wallet';
+import * as bip39 from 'bip39';
+import * as ed25519 from 'ed25519-hd-key';
+import bs58 from 'bs58';
 
-export {
-  MultisigTransaction,
-  Wallet
-};
+export class Wallet {
+  private _seed: Buffer;
+  private _privateKey: Buffer;
+  private _publicKey: Buffer;
+  private _address: string;
+
+  constructor(seed?: string) {
+    if (seed) {
+      this._seed = Buffer.from(bip39.mnemonicToSeedSync(seed));
+    } else {
+      this._seed = bip39.generateMnemonic().then(mnemonic => bip39.mnemonicToSeedSync(mnemonic));
+    }
+
+    const { key: privateKey } = ed25519.derivePath("m/44'/60'/0'/0/0", this._seed);
+    this._privateKey = privateKey;
+    this._publicKey = ed25519.getPublicKey(privateKey);
+    this._address = bs58.encode(this._publicKey);
+  }
+
+  get seed(): string {
+    return bip39.entropyToMnemonic(this._seed);
+  }
+
+  get privateKey(): string {
+    return bs58.encode(this._privateKey);
+  }
+
+  get publicKey(): string {
+    return bs58.encode(this._publicKey);
+  }
+
+  get address(): string {
+    return this._address;
+  }
+}
