165f73aa715ed2ed1c358ff30110b3ebed3727a6
openchain-dev <openchain-dev@users.noreply.github.com>
2026-02-01 12:23:15 +0000

    [CLAW-fe-003] [CLAW] build: Create address page

--- src/claw-generated/AddressPage.tsx
+++ src/claw-generated/AddressPage.tsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { getAddressBalance, getAddressTransactions, getAddressTokens } from '../api/address';
+import { getAddressBalance, getAddressTransactions, getAddressTokens } from './api/blockchain';
+import AddressBalanceCard from './AddressBalanceCard';
+import AddressTokenHoldings from './AddressTokenHoldings';
+import AddressTransactionHistory from './AddressTransactionHistory';
 
 const AddressPage: React.FC = () => {
   const { address } = useParams<{ address: string }>();
@@ -20,23 +23,9 @@ const AddressPage: React.FC = () => {
   return (
     <div>
       <h1>Address: {address}</h1>
-      <p>Balance: {balance} CLAW</p>
-      <h2>Transactions</h2>
-      <ul>
-        {transactions.map((tx) => (
-          <li key={tx.id}>
-            {tx.type} - {tx.amount} CLAW
-          </li>
-        ))}
-      </ul>
-      <h2>Token Holdings</h2>
-      <ul>
-        {tokens.map((token) => (
-          <li key={token.id}>
-            {token.name} - {token.balance}
-          </li>
-        ))}
-      </ul>
+      <AddressBalanceCard balance={balance} />
+      <AddressTokenHoldings tokens={tokens} />
+      <AddressTransactionHistory transactions={transactions} />
     </div>
   );
 };
