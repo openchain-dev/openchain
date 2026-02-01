c9080942cd188c09ecd3168e3be54b95bb70b20a
openchain-dev <openchain-dev@users.noreply.github.com>
2026-02-01 17:23:09 +0000

    [CLAW] Implement multisig wallet and transaction support

--- src/claw-generated/Wallet.ts
+++ src/claw-generated/Wallet.ts
@@ -1,57 +1,38 @@
-import * as bip39 from 'bip39';
-import * as ed25519 from 'ed25519-hd-key';
-import { encode as encodeBase58 } from 'bs58';
+import { KeyPair } from '../crypto';
 
-class Wallet {
-  private _privateKey: Uint8Array;
-  private _publicKey: Uint8Array;
-  private _address: string;
+export class Wallet {
+  private keyPairs: KeyPair[];
+  private requiredSignatures: number;
 
-  constructor(privateKey?: Uint8Array, mnemonic?: string) {
-    if (privateKey) {
-      this._privateKey = privateKey;
-      this._publicKey = Wallet.derivePublicKey(privateKey);
-      this._address = Wallet.deriveAddress(this._publicKey);
-    } else if (mnemonic) {
-      const seed = bip39.mnemonicToSeedSync(mnemonic);
-      const { key: privateKey } = ed25519.derivePath("m/44'/60'/0'/0/0", seed.toString('hex'));
-      this._privateKey = privateKey;
-      this._publicKey = Wallet.derivePublicKey(privateKey);
-      this._address = Wallet.deriveAddress(this._publicKey);
-    } else {
-      const { privateKey, publicKey } = Wallet.generateKeyPair();
-      this._privateKey = privateKey;
-      this._publicKey = publicKey;
-      this._address = Wallet.deriveAddress(publicKey);
+  constructor(keyPairs: KeyPair[], requiredSignatures: number) {
+    this.keyPairs = keyPairs;
+    this.requiredSignatures = requiredSignatures;
+  }
+
+  getPublicKeys(): string[] {
+    return this.keyPairs.map(kp => kp.publicKey);
+  }
+
+  getRequiredSignatures(): number {
+    return this.requiredSignatures;
+  }
+
+  sign(message: string): string[] {
+    const signatures = this.keyPairs.map(kp => kp.sign(message));
+    return signatures;
+  }
+
+  verify(message: string, signatures: string[]): boolean {
+    if (signatures.length < this.requiredSignatures) {
+      return false;
     }
-  }
 
-  get privateKey(): Uint8Array {
-    return this._privateKey;
-  }
+    for (let i = 0; i < this.requiredSignatures; i++) {
+      if (!this.keyPairs[i].verify(message, signatures[i])) {
+        return false;
+      }
+    }
 
-  get publicKey(): Uint8Array {
-    return this._publicKey;
-  }
-
-  get address(): string {
-    return this._address;
-  }
-
-  static generateKeyPair(): { privateKey: Uint8Array; publicKey: Uint8Array } {
-    const seed = bip39.mnemonicToSeedSync('');
-    const { key: privateKey } = ed25519.derivePath("m/44'/60'/0'/0/0", seed.toString('hex'));
-    const publicKey = Wallet.derivePublicKey(privateKey);
-    return { privateKey, publicKey };
-  }
-
-  static derivePublicKey(privateKey: Uint8Array): Uint8Array {
-    return ed25519.getPublicKey(privateKey);
-  }
-
-  static deriveAddress(publicKey: Uint8Array): string {
-    return encodeBase58(publicKey);
+    return true;
   }
 }
-
-export default Wallet;
