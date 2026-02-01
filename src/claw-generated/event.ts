a29f68d2522aed0c3c2dd3b85323a93a9585c0dd
openchain-dev <openchain-dev@users.noreply.github.com>
2026-02-01 10:42:24 +0000

    [CLAW-sc-007] [CLAW] build: Build event emission system

--- src/claw-generated/event.ts
+++ src/claw-generated/event.ts
@@ -1,8 +1,76 @@
+import { ByteArray, Hex } from './types';
+
 export class Event {
+  name: string;
+  parameters: { [key: string]: any };
+
+  constructor(name: string, parameters: { [key: string]: any }) {
+    this.name = name;
+    this.parameters = parameters;
+  }
+
+  toJSON(): { name: string; parameters: { [key: string]: any } } {
+    return {
+      name: this.name,
+      parameters: this.parameters,
+    };
+  }
+
+  static fromJSON(json: { name: string; parameters: { [key: string]: any } }): Event {
+    return new Event(json.name, json.parameters);
+  }
+}
+
+export class EventLog {
+  events: Event[];
+  address: ByteArray;
+  transactionHash: Hex;
+  blockNumber: number;
+  logIndex: number;
+
   constructor(
-    public contractAddress: string,
-    public name: string,
-    public indexedParams: any[],
-    public unindexedParams: any[]
-  ) {}
+    events: Event[],
+    address: ByteArray,
+    transactionHash: Hex,
+    blockNumber: number,
+    logIndex: number
+  ) {
+    this.events = events;
+    this.address = address;
+    this.transactionHash = transactionHash;
+    this.blockNumber = blockNumber;
+    this.logIndex = logIndex;
+  }
+
+  toJSON(): {
+    events: { name: string; parameters: { [key: string]: any } }[];
+    address: ByteArray;
+    transactionHash: Hex;
+    blockNumber: number;
+    logIndex: number;
+  } {
+    return {
+      events: this.events.map((event) => event.toJSON()),
+      address: this.address,
+      transactionHash: this.transactionHash,
+      blockNumber: this.blockNumber,
+      logIndex: this.logIndex,
+    };
+  }
+
+  static fromJSON(json: {
+    events: { name: string; parameters: { [key: string]: any } }[];
+    address: ByteArray;
+    transactionHash: Hex;
+    blockNumber: number;
+    logIndex: number;
+  }): EventLog {
+    return new EventLog(
+      json.events.map(Event.fromJSON),
+      json.address,
+      json.transactionHash,
+      json.blockNumber,
+      json.logIndex
+    );
+  }
 }
