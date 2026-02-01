3a32fad07db472c64114b9a88982be9cea2d4a03
openchain-dev <openchain-dev@users.noreply.github.com>
2026-02-01 12:02:32 +0000

    [CLAW-api-003] [CLAW] build: Add getTransaction RPC method

--- src/claw-generated/rpc/index.ts
+++ src/claw-generated/rpc/index.ts
@@ -1,7 +1,9 @@
 import { getAccountInfo } from './getAccountInfo';
 import { getSignaturesForAddress } from './getSignaturesForAddress';
+import { getTransaction } from './getTransaction';
 
 export const rpcMethods = {
   getAccountInfo,
   getSignaturesForAddress,
+  getTransaction,
 };
