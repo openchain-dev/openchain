d81f6e4feaf97835e3c6c8376febf5dbd1a4a5ea
openchain-dev <openchain-dev@users.noreply.github.com>
2026-02-01 11:17:27 +0000

    [CLAW] Implement GraphQL API layer

--- src/claw-generated/api/graphql.ts
+++ src/claw-generated/api/graphql.ts
@@ -2,47 +2,145 @@ import { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLLis
 import { Block, Transaction, Account, Contract, Staking, Governance, Proposal, Vote } from './models';
 import { getBlock, getTransaction, getAccount, getContract, getStakingInfo, getGovernanceInfo, getTransactionsByAddress } from '../services/graphql.service';
 
+const BlockType = new GraphQLObjectType({
+  name: 'Block',
+  fields: {
+    number: { type: GraphQLInt },
+    hash: { type: GraphQLString },
+    parentHash: { type: GraphQLString },
+    timestamp: { type: GraphQLInt },
+    transactions: {
+      type: new GraphQLList(TransactionType),
+      resolve: (block) => getTransactionsByAddress(block.hash)
+    }
+  }
+});
+
+const TransactionType = new GraphQLObjectType({
+  name: 'Transaction',
+  fields: {
+    hash: { type: GraphQLString },
+    from: { type: GraphQLString },
+    to: { type: GraphQLString },
+    value: { type: GraphQLInt },
+    gas: { type: GraphQLInt },
+    gasPrice: { type: GraphQLInt },
+    input: { type: GraphQLString },
+    blockNumber: { type: GraphQLInt },
+    timestamp: { type: GraphQLInt }
+  }
+});
+
+const AccountType = new GraphQLObjectType({
+  name: 'Account',
+  fields: {
+    address: { type: GraphQLString },
+    balance: { type: GraphQLInt },
+    nonce: { type: GraphQLInt },
+    code: { type: GraphQLString },
+    transactions: {
+      type: new GraphQLList(TransactionType),
+      resolve: (account) => getTransactionsByAddress(account.address)
+    }
+  }
+});
+
+const ContractType = new GraphQLObjectType({
+  name: 'Contract',
+  fields: {
+    address: { type: GraphQLString },
+    abi: { type: GraphQLString },
+    bytecode: { type: GraphQLString }
+  }
+});
+
+const StakingType = new GraphQLObjectType({
+  name: 'Staking',
+  fields: {
+    totalStaked: { type: GraphQLInt },
+    rewardRate: { type: GraphQLInt },
+    stakingPools: { type: new GraphQLList(StakingPoolType) }
+  }
+});
+
+const StakingPoolType = new GraphQLObjectType({
+  name: 'StakingPool',
+  fields: {
+    address: { type: GraphQLString },
+    totalStaked: { type: GraphQLInt },
+    apy: { type: GraphQLInt }
+  }
+});
+
+const GovernanceType = new GraphQLObjectType({
+  name: 'Governance',
+  fields: {
+    proposals: { type: new GraphQLList(ProposalType) },
+    votes: { type: new GraphQLList(VoteType) }
+  }
+});
+
+const ProposalType = new GraphQLObjectType({
+  name: 'Proposal',
+  fields: {
+    id: { type: GraphQLInt },
+    title: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+    votes: { type: new GraphQLList(VoteType) }
+  }
+});
+
+const VoteType = new GraphQLObjectType({
+  name: 'Vote',
+  fields: {
+    voter: { type: GraphQLString },
+    proposal: { type: GraphQLInt },
+    support: { type: GraphQLString }
+  }
+});
+
 const RootQueryType = new GraphQLObjectType({
   name: 'Query',
   fields: {
     block: {
-      type: Block,
+      type: BlockType,
       args: {
         number: { type: GraphQLInt }
       },
       resolve: (_, { number }) => getBlock(number)
     },
     transaction: {
-      type: Transaction,
+      type: TransactionType,
       args: {
         hash: { type: GraphQLString }
       },
       resolve: (_, { hash }) => getTransaction(hash)
     },
     account: {
-      type: Account,
+      type: AccountType,
       args: {
         address: { type: GraphQLString }
       },
       resolve: (_, { address }) => getAccount(address)
     },
     contract: {
-      type: Contract,
+      type: ContractType,
       args: {
         address: { type: GraphQLString }
       },
       resolve: (_, { address }) => getContract(address)
     },
     staking: {
-      type: Staking,
+      type: StakingType,
       resolve: () => getStakingInfo()
     },
     governance: {
-      type: Governance,
+      type: GovernanceType,
       resolve: () => getGovernanceInfo()
     },
     transactionsByAddress: {
-      type: new GraphQLList(Transaction),
+      type: new GraphQLList(TransactionType),
       args: {
         address: { type: GraphQLString }
       },
--- src/claw-generated/services/graphql.service.ts
+++ src/claw-generated/services/graphql.service.ts
@@ -1,30 +1,111 @@
 import { Block, Transaction, Account, Contract, Staking, Governance, Proposal, Vote } from '../api/models';
-import { getBlockByNumber, getTransactionByHash, getAccountByAddress, getContractByAddress, getStakingInfo, getGovernanceInfo, getTransactionsByAddress } from '../blockchain';
+import { BlockManager } from '../BlockManager';
+import { AccountManager } from '../AccountManager';
+import { ContractStorage } from '../ContractStorage';
+import { StakingManager } from '../StakingManager';
+import { GovernanceManager } from '../GovernanceManager';
 
-export const getBlock = async (number: number): Promise<Block> => {
-  return await getBlockByNumber(number);
-};
+export async function getBlock(number: number): Promise<Block> {
+  const block = await BlockManager.getBlock(number);
+  return {
+    number: block.number,
+    hash: block.hash,
+    parentHash: block.parentHash,
+    timestamp: block.timestamp,
+    transactions: block.transactions.map(tx => ({
+      hash: tx.hash,
+      from: tx.from,
+      to: tx.to,
+      value: tx.value,
+      gas: tx.gas,
+      gasPrice: tx.gasPrice,
+      input: tx.input,
+      blockNumber: block.number,
+      timestamp: block.timestamp
+    }))
+  };
+}
 
-export const getTransaction = async (hash: string): Promise<Transaction> => {
-  return await getTransactionByHash(hash);
-};
+export async function getTransaction(hash: string): Promise<Transaction> {
+  const tx = await BlockManager.getTransaction(hash);
+  return {
+    hash: tx.hash,
+    from: tx.from,
+    to: tx.to,
+    value: tx.value,
+    gas: tx.gas,
+    gasPrice: tx.gasPrice,
+    input: tx.input,
+    blockNumber: tx.blockNumber,
+    timestamp: tx.timestamp
+  };
+}
 
-export const getAccount = async (address: string): Promise<Account> => {
-  return await getAccountByAddress(address);
-};
+export async function getAccount(address: string): Promise<Account> {
+  const account = await AccountManager.getAccount(address);
+  return {
+    address: account.address,
+    balance: account.balance,
+    nonce: account.nonce,
+    code: account.code
+  };
+}
 
-export const getContract = async (address: string): Promise<Contract> => {
-  return await getContractByAddress(address);
-};
+export async function getContract(address: string): Promise<Contract> {
+  const contract = await ContractStorage.getContract(address);
+  return {
+    address: contract.address,
+    abi: contract.abi,
+    bytecode: contract.bytecode
+  };
+}
 
-export const getStakingInfo = async (): Promise<Staking> => {
-  return await getStakingInfo();
-};
+export async function getStakingInfo(): Promise<Staking> {
+  const { totalStaked, rewardRate, stakingPools } = await StakingManager.getStakingInfo();
+  return {
+    totalStaked,
+    rewardRate,
+    stakingPools: stakingPools.map(pool => ({
+      address: pool.address,
+      totalStaked: pool.totalStaked,
+      apy: pool.apy
+    }))
+  };
+}
 
-export const getGovernanceInfo = async (): Promise<Governance> => {
-  return await getGovernanceInfo();
-};
+export async function getGovernanceInfo(): Promise<Governance> {
+  const { proposals, votes } = await GovernanceManager.getGovernanceInfo();
+  return {
+    proposals: proposals.map(proposal => ({
+      id: proposal.id,
+      title: proposal.title,
+      description: proposal.description,
+      status: proposal.status,
+      votes: votes.filter(vote => vote.proposal === proposal.id).map(vote => ({
+        voter: vote.voter,
+        proposal: vote.proposal,
+        support: vote.support
+      }))
+    })),
+    votes: votes.map(vote => ({
+      voter: vote.voter,
+      proposal: vote.proposal,
+      support: vote.support
+    }))
+  };
+}
 
-export const getTransactionsByAddress = async (address: string): Promise<Transaction[]> => {
-  return await getTransactionsByAddress(address);
-};
+export async function getTransactionsByAddress(address: string): Promise<Transaction[]> {
+  const txs = await BlockManager.getTransactionsByAddress(address);
+  return txs.map(tx => ({
+    hash: tx.hash,
+    from: tx.from,
+    to: tx.to,
+    value: tx.value,
+    gas: tx.gas,
+    gasPrice: tx.gasPrice,
+    input: tx.input,
+    blockNumber: tx.blockNumber,
+    timestamp: tx.timestamp
+  }));
+}
