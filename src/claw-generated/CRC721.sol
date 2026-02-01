da683b67eabfe2582cde274a514ddcaa11dd9b64
openchain-dev <openchain-dev@users.noreply.github.com>
2026-02-01 05:59:56 +0000

    [CLAW] Implement CRC-721 NFT standard

--- src/claw-generated/CRC721.sol
+++ src/claw-generated/CRC721.sol
@@ -2,76 +2,35 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-/**
- * @title CRC721 Non-Fungible Token Standard
- * @dev Implementation of the CRC-721 non-fungible token standard.
- */
-contract CRC721 is ERC721, ERC721Enumerable, ERC721URIStorage {
+contract CRC721 is ERC721, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
 
+    mapping(uint256 => string) private _tokenURIs;
+
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
 
-    /**
-     * @dev Mints a new NFT with the given tokenURI.
-     * @param to The address to mint the NFT to.
-     * @param tokenURI The metadata URI for the new NFT.
-     * @return The new token ID.
-     */
-    function mint(address to, string memory tokenURI) public returns (uint256) {
+    function safeMint(address to) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
-        _setTokenURI(tokenId, tokenURI);
-        return tokenId;
     }
 
-    /**
-     * @dev Burns the NFT with the given tokenId.
-     * @param tokenId The ID of the NFT to burn.
-     */
     function burn(uint256 tokenId) public {
-        require(_exists(tokenId), "CRC721: token does not exist");
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "CRC721: caller is not owner nor approved");
         _burn(tokenId);
     }
 
-    // Implement other CRC-721 functionality here, such as transfers, approvals, etc.
-
-    // The following functions are overrides required by Solidity.
-
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
-        super._beforeTokenTransfer(from, to, tokenId);
+    function setTokenURI(uint256 tokenId, string memory tokenURI) public onlyOwner {
+        require(_exists(tokenId), "CRC721: URI set of nonexistent token");
+        _tokenURIs[tokenId] = tokenURI;
     }
 
-    function _burn(uint256 tokenId)
-        internal
-        override(ERC721, ERC721URIStorage)
-    {
-        super._burn(tokenId);
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "CRC721: URI query for nonexistent token");
+        return _tokenURIs[tokenId];
     }
 }
